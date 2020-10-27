aa20f07566b027b484ccdaedf228a76cd8abb849
Agnieszka Herod <agn.j.herod@gmail.com>
2020-10-27 21:35:06 +0100

    netlify CMS setup

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,34 @@
 // This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Martyna Jonca - Marketing Specialist',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+    }
+  },
+    plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
+  ]
 }
